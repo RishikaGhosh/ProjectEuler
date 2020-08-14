3182caf060b1abf8ee039c98c5540606299a9f95
Rishika Ghosh <58912231+RishikaGhosh@users.noreply.github.com>
2020-08-14 14:32:50 +0530

    Add files via upload

---
+++ projecteuler_15.cpp
@@ -0,0 +1,8 @@
+//no 15
+/*
+we see that for a grid size 2 the results are rrdd rdrd rddr drrd drdr ddrr
+so in each step we see there are 2n steps
+2 for the right move,2 for the left move
+so for a 2x2 grid,there are 4c2 possible ways
+ 
+SO,for a 20x20 grid the answer is 40C20*/
